2c8f11fd9d5fbaec3399eb79ef6ea9dc2ad89c38
yamatakau08 <yamatakau08@gmail.com>
2025-12-11 17:59:17 +0900

    Initial commit

---
+++ init.lua
@@ -0,0 +1,25 @@
+local function setInputSource(appName, inputSource)
+    hs.keycodes.setLayout(inputSource)
+end
+
+local englishApps = {
+      ["ターミナル"] = true,
+      ["Emacs"] = true,
+      ["WezTerm"] = true,
+}
+
+local japaneseApps = {
+      ["Obsidian"] = true,
+}
+
+local appWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+    if eventType == hs.application.watcher.activated then
+        if englishApps[appName] then
+            hs.keycodes.setLayout("ABC") -- should use `setLayout` 英語入力に切り替え
+        elseif japaneseApps[appName] then
+            hs.keycodes.setMethod("Hiragana") -- should use `setMethod` 日本語入力に切り替え
+	end
+    end
+end)
+
+appWatcher:start()
